7ff9951eefd71c8c2313b27526271c03ac221185
BunbunTheVile <evolvingbunny@gmail.com>
2023-08-04 15:41:43 +0200

    amend

--- frontend/spacewise/src/app/app.component.ts
+++ frontend/spacewise/src/app/app.component.ts
@@ -73,8 +73,6 @@ export class AppComponent implements OnInit {
 
       this.userService.updateNotesForUser(this.loginService.currentUser.id!, notesValue).subscribe();
     });
-
-    this.notificationService.showNotification("Achtung! Das hier ist eine Notification!", NotificationColour.red);
   }
 
   private updateNotepadWithUserNotes() {
